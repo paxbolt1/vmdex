28c5af4cd9e55509b53d1e4e2c7a0d1a0f30ad0f
Paxbolt <paxbolt1@gmail.com>
2025-12-21 13:37:54 +0530

    vmdex build 01CZ

---
+++ dev.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }: {
+  channel = "stable-24.05";
+
+  packages = with pkgs; [
+    unzip
+    openssh
+    git
+    qemu_kvm
+    sudo
+    cdrkit
+    cloud-utils
+    qemu
+    wget
+  ];
+
+  env = {
+    EDITOR = "nano";
+  };
+
+  idx = {
+    extensions = [
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
+    ];
+
+    workspace = {
+      onCreate = { };
+      onStart = { };
+    };
+
+    previews = {
+      enable = false;
+    };
+  };
+}
